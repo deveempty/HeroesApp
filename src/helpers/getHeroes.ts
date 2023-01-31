3a83b8870bbe9892e94d0446550d95fcd0ca33c3
Juan Viloria <juanviloriav@gmail.com>
2023-01-30 19:05:40 -0600

    changes to deploy on vercel

--- src/helpers/getHeroes.ts
+++ src/helpers/getHeroes.ts
@@ -2,7 +2,7 @@ import { heroesApi } from "../api/heroesApi"
 
 export const getHeroes = async ({heroes}: {heroes:string}) => {
 
-    const { data } = await heroesApi.get(`/api/125744090398388/search/${heroes}`);
+    const { data } = await heroesApi.get(`/api.php/${process.env.REACT_APP_MY_KEY}/search/${heroes}`);
     return {
         data: data.results,
         status: data.response
